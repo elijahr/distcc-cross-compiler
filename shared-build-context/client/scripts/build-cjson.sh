af90ffaa2c0458bd12caa6aa104a6e4db0be2a43
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-10-23 01:21:35 -0500

    fix test arg

--- shared-build-context/client/scripts/build-cjson.sh
+++ shared-build-context/client/scripts/build-cjson.sh
@@ -6,9 +6,9 @@ main () {
   cd $(dirname $0)
 
   # gcc/etc should use ccache first
-  test $(which gcc) == "/usr/lib/ccache/gcc" || test $(which gcc) == "/usr/lib/ccache/bin/gcc"
-  test $(which g++) == "/usr/lib/ccache/g++" || test $(which g++) == "/usr/lib/ccache/bin/g++"
-  test $(which cc) == "/usr/lib/ccache/cc" || test $(which cc) == "/usr/lib/ccache/bin/cc"
+  test "$(which gcc)" == "/usr/lib/ccache/gcc" || test "$(which gcc)" == "/usr/lib/ccache/bin/gcc"
+  test "$(which g++)" == "/usr/lib/ccache/g++" || test "$(which g++)" == "/usr/lib/ccache/bin/g++"
+  test "$(which cc)" == "/usr/lib/ccache/cc" || test "$(which cc)" == "/usr/lib/ccache/bin/cc"
 
   # Assert that ccache wrappers wrap distcc wrappers
   [[ "$((gcc 2>&1 || true) | tail -n 1)" =~ ^distcc[\[0-9\]+] ]]
